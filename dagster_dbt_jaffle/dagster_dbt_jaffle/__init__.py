38a0b27887dee974609582f9a26a337bc9c37b1a
danhphan <danh.phan.mq@gmail.com>
2023-04-06 12:43:42 +1000

    Add dbt assets into dagster

--- dagster_dbt_jaffle/dagster_dbt_jaffle/__init__.py
+++ dagster_dbt_jaffle/dagster_dbt_jaffle/__init__.py
@@ -1,5 +1,22 @@
-from dagster import Definitions, load_assets_from_modules
+import os
+from dagster import Definitions, load_assets_from_modules, file_relative_path
+from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
 
-from . import assets
 
-defs = Definitions(assets=load_assets_from_modules([assets]))
+DBT_PROJECT_PATH = file_relative_path(__file__, "../../dbt/jaffle_shop")
+DBT_PROFILES = file_relative_path(__file__, "../../dbt/jaffle_shop")
+
+dbt_assets = load_assets_from_dbt_project(
+    project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["jaffle_analytics"]
+)
+
+resources = {
+    "dbt": dbt_cli_resource.configured(
+        {
+            "project_dir": DBT_PROJECT_PATH,
+            "profiles_dir": DBT_PROFILES,
+        },
+    ),
+}
+
+defs = Definitions(assets=[*dbt_assets], resources=resources)
