befa31dbc56bcec1c54f04eb52794e9e27057143
danhphan <danh.phan.mq@gmail.com>
2023-04-06 11:35:02 +1000

    Add code for EL process

---
+++ dbt/jaffle_shop/extract_load.py
@@ -0,0 +1,17 @@
+import duckdb as db
+
+conn = db.connect('jaffle_shop.duckdb')
+
+
+conn.execute('CREATE SCHEMA IF NOT EXISTS jaffle_raw')
+
+conn.execute('USE jaffle_raw')
+
+conn.execute('CREATE TABLE IF NOT EXISTS raw_customers AS SELECT * FROM "./data/raw_customers.csv"')
+
+conn.execute('CREATE TABLE IF NOT EXISTS raw_orders AS SELECT * FROM "./data/raw_orders.csv"')
+
+conn.execute('CREATE TABLE IF NOT EXISTS raw_payments AS SELECT * FROM "./data/raw_payments.csv"')
+
+
+conn.sql("SHOW TABLES")
