01a121c0e20d52fd27a0c6553ab20dece8fbd8b8
Danh Phan <tphan@hesta.com.au>
2023-07-09 07:37:51 +0000

    update duckdb version

--- dbt/jaffle_shop/extract_load.py
+++ dbt/jaffle_shop/extract_load.py
@@ -2,16 +2,26 @@ import duckdb as db
 
 conn = db.connect('jaffle_shop_db.duckdb')
 
-
-conn.execute('CREATE SCHEMA IF NOT EXISTS jaffle_raw')
+conn.execute('CREATE SCHEMA jaffle_raw')
 
 conn.execute('USE jaffle_raw')
 
-conn.execute('CREATE TABLE IF NOT EXISTS raw_customers AS SELECT * FROM "./data/raw_customers.csv"')
+conn.execute('CREATE TABLE raw_customers AS SELECT * FROM "./data/raw_customers.csv"')
 
-conn.execute('CREATE TABLE IF NOT EXISTS raw_orders AS SELECT * FROM "./data/raw_orders.csv"')
+conn.execute('CREATE TABLE raw_orders AS SELECT * FROM "./data/raw_orders.csv"')
 
-conn.execute('CREATE TABLE IF NOT EXISTS raw_payments AS SELECT * FROM "./data/raw_payments.csv"')
+conn.execute('CREATE TABLE raw_payments AS SELECT * FROM "./data/raw_payments.csv"')
+
+
+# conn.execute('CREATE SCHEMA IF NOT EXISTS jaffle_raw')
+
+# conn.execute('USE jaffle_raw')
+
+# conn.execute('CREATE TABLE IF NOT EXISTS raw_customers AS SELECT * FROM "./data/raw_customers.csv"')
+
+# conn.execute('CREATE TABLE IF NOT EXISTS raw_orders AS SELECT * FROM "./data/raw_orders.csv"')
+
+# conn.execute('CREATE TABLE IF NOT EXISTS raw_payments AS SELECT * FROM "./data/raw_payments.csv"')
 
 
 conn.sql("SHOW TABLES")
