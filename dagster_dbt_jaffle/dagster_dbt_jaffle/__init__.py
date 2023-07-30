ad55abf03db948aaa3e3ce91306b116c9dce9e12
danhphan <danh.phan.mq@gmail.com>
2023-07-30 13:46:42 +1000

    add raw_data assets

--- dagster_dbt_jaffle/dagster_dbt_jaffle/__init__.py
+++ dagster_dbt_jaffle/dagster_dbt_jaffle/__init__.py
@@ -1,13 +1,21 @@
 import os
-from dagster import Definitions, load_assets_from_modules, file_relative_path
+from dagster import Definitions, file_relative_path
+from dagster import load_assets_from_modules, load_assets_from_package_module
 from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
-
+from dagster_dbt_jaffle.assets import raw_data
 
 DBT_PROJECT_PATH = file_relative_path(__file__, "../../dbt_jaffle_shop")
 DBT_PROFILES = file_relative_path(__file__, "../../dbt_jaffle_shop")
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["jaffle_analytics"]
+)
+
+raw_data_assets = load_assets_from_package_module(
+    raw_data,
+    group_name="raw_data",
+    # all of these assets live in the duckdb database, under the schema raw_data
+    key_prefix=["duckdb", "raw_data"],
 )
 
 resources = {
@@ -19,4 +27,4 @@ resources = {
     ),
 }
 
-defs = Definitions(assets=[*dbt_assets], resources=resources)
+defs = Definitions(assets=[*dbt_assets, *raw_data_assets], resources=resources)
