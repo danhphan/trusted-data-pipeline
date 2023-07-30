de52653b71d7cf9ca8b24e8707a33ce2982f9aeb
danhphan <danh.phan.mq@gmail.com>
2023-07-30 13:28:23 +1000

    add extract load for customers

--- dbt_jaffle_shop/extract_load.py
+++ dbt_jaffle_shop/extract_load.py
@@ -1,10 +1,30 @@
 import duckdb as db
+import pandas as pd
+
+def create_raw_scheme():
+    conn = db.connect('jaffle_shop_db.duckdb')
+    conn.execute('CREATE SCHEMA IF NOT EXISTS jaffle_raw')
+    conn.execute('USE jaffle_raw')
+    return conn
+
 
 conn = db.connect('jaffle_shop_db.duckdb')
 
 conn.execute('CREATE SCHEMA jaffle_raw')
 
 conn.execute('USE jaffle_raw')
+
+
+def extract_raw_customers():
+    df = pd.read_csv("s3://dbt-tutorial-public/jaffle_shop_customers.csv")
+    return df
+
+
+def load_raw_customers():
+    conn = create_raw_scheme()
+    conn.execute('CREATE TABLE raw_customers AS SELECT * FROM "./data/raw_customers.csv"')
+
+
 
 conn.execute('CREATE TABLE raw_customers AS SELECT * FROM "./data/raw_customers.csv"')
 
