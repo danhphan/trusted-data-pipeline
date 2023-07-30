2ca4b606ec615060085245a16f22cc2f77b7314f
danhphan <danh.phan.mq@gmail.com>
2023-07-30 13:22:42 +1000

    update the path of dbt project

--- dagster_dbt_jaffle/dagster_dbt_jaffle/__init__.py
+++ dagster_dbt_jaffle/dagster_dbt_jaffle/__init__.py
@@ -3,8 +3,8 @@ from dagster import Definitions, load_assets_from_modules, file_relative_path
 from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
 
 
-DBT_PROJECT_PATH = file_relative_path(__file__, "../../dbt/jaffle_shop")
-DBT_PROFILES = file_relative_path(__file__, "../../dbt/jaffle_shop")
+DBT_PROJECT_PATH = file_relative_path(__file__, "../../dbt_jaffle_shop")
+DBT_PROFILES = file_relative_path(__file__, "../../dbt_jaffle_shop")
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["jaffle_analytics"]
